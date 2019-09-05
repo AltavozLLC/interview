809c3aac92768029ac8c0cd39b10fafadfd6c877
Evan Mallory <esopian@gmail.com>
2019-09-05 13:56:25 -0500

    question 2

---
+++ __tests__/002.js
@@ -0,0 +1,15 @@
+import {
+  getNames,
+  getValues,
+  getSortedUniqueValues,
+  getPropsB,
+  getIndexedObject,
+  getNamesFromObjects
+} from "../questions/002";
+import { isEqualArray } from "../_utils/array";
+
+describe("Question 002", () => {
+  describe('getNames', () => {
+    it('basic', () => {});
+  })
+});
---
+++ questions/002.js
@@ -0,0 +1,72 @@
+/*
+Given the following data answer the questions below
+*/
+
+const items = [
+  {
+    name: "item 1",
+    id: 2,
+    props: {
+      a: "a prop1",
+      b: "b prop1"
+    },
+    values: [1, 2, 3]
+  },
+  {
+    name: "item 2",
+    id: 3,
+    props: {
+      a: "a prop2",
+      b: "b prop2"
+    },
+    values: [6, 1, 2, 3, 4]
+  },
+  {
+    name: "item 3",
+    id: 4,
+    props: {
+      a: "a prop3",
+      c: "c prop3"
+    },
+    values: [10, 1, 2, 3, 4, 5]
+  }
+];
+
+// Create a function that returns an array of all item names.
+export function getNames(items) {
+  // write code here
+}
+
+// Create a function that returns an array of values (not array of arrays).
+export function getValues(items) {
+  // write code here
+}
+
+// Create a function that returns a sorted and unique array of values.
+export function getSortedUniqueValues(items) {
+  // write code here
+}
+
+//  Create a function that returns an array of all the values of property 'b'.
+export function getPropsB(items) {
+  // write code here
+}
+
+// Create a function that returns a plain javascript object that is indexed by name, and contains all the props.
+export function getIndexedObject(items) {
+  // write code here
+}
+
+// Create a function that returns an array of all the keys returned by getIndexedObject (from previous exercise)
+export function getNamesFromObjects(items) {
+  // write code here
+}
+
+
+
+
+
+
+
+
+
