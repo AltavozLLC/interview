dd5c5d6981d6abbb83f326785881d283552e1b3c
Jeremiah Brown <soybuddha@gmail.com>
2019-09-11 09:06:37 -0500

    fixing test number 3

--- __tests__/003.js
+++ __tests__/003.js
@@ -8,7 +8,7 @@ describe("Question 003", () => {
     const inputA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     const inputB = [2, 4, 6, 0];
     const expected = [1, 3, 5, 7, 8, 9];
-    const actual = difference(items);
+    const actual = difference(inputA, inputB);
 
     expect(isEqualArray(expected.sort(), actual.sort())).toBeTruthy();
   });
